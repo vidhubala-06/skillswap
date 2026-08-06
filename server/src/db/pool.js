82a2c0934c1658dbe6c64d04c84ac18ae377d394
Vidhubala Gunasekaran Ananthan <vidhuananth06@gmail.com>
2026-08-06 21:14:06 +0530

    Update database connection for Aiven (host, port, SSL)

--- server/src/db/pool.js
+++ server/src/db/pool.js
@@ -3,13 +3,17 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  dateStrings: true
+  dateStrings: true,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
